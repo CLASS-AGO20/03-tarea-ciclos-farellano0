72aa1d7cb94e233c7e78dc6f5a332f75053f37d7
Fernando Arellano <farellano0@ucol.mx>
2020-12-08 20:00:27 -0600

    Implemented sumatoriaSerieUno()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero){
+        let suma = 0;
+        let denominador = 2;
+
+        for(let i = 1; i >= 1/numero; i = 1/denominador ++) {
+            suma = suma + i
+            console.log(suma)
+        }
+        return suma;
+    }
+
+}
 
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(15));
