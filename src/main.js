3491cb0841f9aafec5f1c07c71943f0a3f565ad0
Fernando Arellano <farellano0@ucol.mx>
2020-12-08 23:27:10 -0600

    Implemented sumatoriaSerieDos()

--- src/main.js
+++ src/main.js
@@ -5,14 +5,31 @@ export default class App {
         let denominador = 2;
 
         for(let i = 1; i >= 1/numero; i = 1/denominador ++) {
-            suma = suma + i
-            console.log(suma)
+            suma = suma + i;
         }
         return suma;
     }
+    sumatoriaSerieDos(numero){
+        let suma = 0;
+        let denominador = 1
+        let i = 1/denominador;
+
+        while(i >= 1/numero){
+            suma = suma + i;
+            denominador ++;
+
+        }
+        return suma;
+    }
+
 
 }
 
 let app = new App();
 
+console.log("Probando sumatoriaSerieUno()");
 console.log(app.sumatoriaSerieUno(15));
+
+console.log("Probando sumatoriaSerieDos()");
+console.log(app.sumatoriaSerieDos(15));
+
